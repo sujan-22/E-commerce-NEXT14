fff12f8a9f5b1cbaf709a91f1e5a8882a1483039
Jay Patel <jaykoladia@gmail.com>
2024-10-15 21:24:21 -0400

    add .

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,25 +5,23 @@ import Product from "@/hooks/useProducts";
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
 
 export const metadata = {
-    title: "Fashion E-commerce",
-    description: "A modern fashion e-commerce website",
+  title: "Fashion E-commerce",
+  description: "A modern fashion e-commerce website.",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body className={`font-sans`}>
-                <ReactQueryProvider>
-                    <Navbar />
-                    <main className="flex flex-col min-h-[calc(100vh-2.5rem-1px)] pt-[104.8px]">
-                        <div className="flex flex-1 flex-col h-full">
-                            {children}
-                        </div>
-                        <Footer />
-                    </main>
-                    <Product />
-                </ReactQueryProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={`font-sans`}>
+        <ReactQueryProvider>
+          <Navbar />
+          <main className="flex flex-col min-h-[calc(100vh-2.5rem-1px)] pt-[104.8px]">
+            <div className="flex flex-1 flex-col h-full">{children}</div>
+            <Footer />
+          </main>
+          <Product />
+        </ReactQueryProvider>
+      </body>
+    </html>
+  );
 }
