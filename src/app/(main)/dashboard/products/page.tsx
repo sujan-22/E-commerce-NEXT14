d44cedf8389457050553a8ba409bd2669e347d46
Sujan <sujanrokad44@gmail.com>
2024-12-29 01:29:06 -0500

    fix product collection column in dashboard/products page

--- src/app/(main)/dashboard/products/page.tsx
+++ src/app/(main)/dashboard/products/page.tsx
@@ -118,7 +118,7 @@ const columns: ColumnDef<Product>[] = [
         cell: ({ row }) => {
             const collection: Collection[] = row.getValue("collection");
             const relevantCollections = Object.entries(collection)
-                .filter(([value]) => Object.keys(value).length > 0)
+                .filter(([_, value]) => Object.keys(value).length > 0)
                 .map(([key]) => key);
             return (
                 <div className="capitalize">
